6f3c713134abac5c919b0c1be4b4b1f951a74142
nupustas <vejasprastienis@gmail.com>
2025-09-28 19:34:56 +0300

    Prideta collision check funkcija

--- main.cpp
+++ main.cpp
@@ -1,25 +1,57 @@
-
 #include <iostream>
 #include <string>
 #include <iomanip>
 #include <array>
 #include <cstdint>
 #include <sstream>
+#include <cstdlib>
+#include <ctime>
 
-using std::string;
-using std::cout;
-using std::cin;
-using std::endl;
-
+using namespace std;
 string hashas(const std::string &input);
+int collisions(int length);
+string generateString(int length);
+
 
 int main() {
-    string input;
-    cout << "Enter password" << endl;
-    cin >> input;
 
-    string hashed = hashas(input);
-    cout << "Hashed password: " << hashed << endl;
+    srand(time(0));
+
+    cout<<"(1) Input string"<<endl;
+    cout<<"(2) Input from a file"<<endl;
+    cout<<"(3) Check for collisions"<<endl;
+
+    int choice;
+    cin >> choice;
+
+    switch (choice) {
+        case 1: {
+            string input;
+            cout << "Enter string to hash: ";
+            cin >> input;
+            string hashed = hashas(input);
+            cout << "Hashed string: " << hashed << endl;
+            break;
+        }
+        case 2: {
+            string filename;
+            cout << "Enter filename: ";
+            cin >> filename;
+
+            break;
+        }
+        case 3: {
+            int length;
+            cout << "Enter length of strings to compare: ";
+            cin >> length;
+            int collisionCount = collisions(length);
+            cout << "Number of collisions: " << collisionCount << endl;
+            break;
+        }
+        default:
+            cout << "Invalid choice." << endl;
+            break;
+    }
     return 0;
 }
 
@@ -51,4 +83,29 @@ string hashas(const std::string &input) {
         of << std::hex << std::setw(16) << std::setfill('0') << v;
     }
     return of.str();
+}
+int collisions(int length)
+{   
+    int count = 0;
+    for(int i=0;i<100000;i++)
+    {
+        string h1 = hashas(generateString(length));
+        string h2 = hashas(generateString(length));
+        if(h1 == h2)count++;
+
+    }
+    return count;
+}
+
+string generateString(int length) {
+
+    string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    string result = "";
+    
+    for (int i = 0; i < length; i++) {
+        int randomIndex = rand() % chars.length();  
+        result += chars[randomIndex];
+    }
+    
+    return result;
 }
